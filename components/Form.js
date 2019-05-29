2d0c534180f828fc4644717422ec363b7a807543
Lauren Mason <lauren@basisresearch.co.uk>
2019-05-29 20:14:18 +0100

    Add form

--- components/Form.js
+++ components/Form.js
@@ -1,15 +1,20 @@
-const Form = () => {
-    return (
-        <form onsubmit="handleFormSubmit(this)" action="/" method="POST">
+const Form = (props) => {
+
+    let fields = ['firstName', 'lastName', 'address'];
+
+    let formFields = fields.map(function(field){
+        return (
             <div class="form-group">
-                <label for="InputFirstName">First Name</label>
-                <input class="form-control" id="InputFirstName" name="InputFirstName" placeholder="First Name"
+                <label for={"Input" + field}>{field}</label>
+                <input class="form-control" id={"Input" + field} name={"Input" + field} placeholder="First Name"
                     type="text" />
             </div>
-            <div class="form-group">
-                <label for="InputLastName">Last Name</label>
-                <input class="form-control" id="InputLastName" name="InputLastName" placeholder="Last Name" type="text" />
-            </div>
+        );
+    });
+
+    return (
+        <form onsubmit="handleFormSubmit(this)" action="/" method="POST">
+            {formFields}
             <input id="idfromurl" name="idfromurl" type="hidden" />
             <br />
             <button type="submit" class="btn btn-primary">Submit</button>
